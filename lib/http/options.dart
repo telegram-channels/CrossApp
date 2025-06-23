4f4131ecafa24bcdefa89525cfcbf6b00d246ae3
telegram-channels <laibaidu@hotmail.com>
2025-06-24 06:01:20 +0800

    Update options.dart

--- lib/http/options.dart
+++ lib/http/options.dart
@@ -2,6 +2,6 @@
 
 class HttpOptions {
 //  替换为您的v2board地址和客户端名称
-  static const String websiteurl = 'https://www.strongswans.net/';
-  static const String appName = 'v2b客户端';
+  static const String websiteurl = 'https://88cloud.dpdns.org/';
+  static const String appName = '88云加速';
 }
