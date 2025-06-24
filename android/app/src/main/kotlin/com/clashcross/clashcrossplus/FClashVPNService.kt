365035a0753c1721f0be552532ae70bcba2ecfd6
telegram-channels <laibaidu@hotmail.com>
2025-06-24 11:46:59 +0800

    Update FClashVPNService.kt

--- android/app/src/main/kotlin/com/clashcross/clashcrossplus/FClashVPNService.kt
+++ android/app/src/main/kotlin/com/clashcross/clashcrossplus/FClashVPNService.kt
@@ -1,6 +1,5 @@
 package com.clashcross.clashcrossplus
 
-import alihoseinpoor.com.open_settings.OpenSettingsPlugin
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
@@ -11,20 +10,6 @@ import android.net.VpnService
 import android.os.ParcelFileDescriptor
 import android.util.Log
 import androidx.core.app.NotificationCompat
-//import com.baseflow.permissionhandler.PermissionHandlerPlugin
-import com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin
-import com.kaivean.system_proxy.SystemProxyPlugin
-//import com.mr.flutter.plugin.filepicker.FilePickerPlugin
-//import com.tekartik.sqflite.SqflitePlugin
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin
-//import io.flutter.plugins.imagepicker.ImagePickerPlugin
-import io.flutter.plugins.pathprovider.PathProviderPlugin
-import io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin
-import io.flutter.plugins.urllauncher.UrlLauncherPlugin
-//import io.flutter.plugins.webviewflutter.WebViewFlutterPlugin
-//import io.github.ponnamkarthik.toast.fluttertoast.FlutterToastPlugin
 import java.net.Socket
 
 class FClashVPNService : VpnService() {
@@ -52,8 +37,8 @@ class FClashVPNService : VpnService() {
         val channel = NotificationChannel(CHANNEL, "crazecloud", NotificationManager.IMPORTANCE_HIGH)
         (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(channel)
         val notification = with(NotificationCompat.Builder(this, CHANNEL)) {
-//            setContentTitle("狂飙云主程序")
-//            setContentText("狂飙云主程序正在运行")
+            // setContentTitle("Your Title")
+            // setContentText("Your text")
             build()
         }
         startForeground(1, notification)
@@ -63,7 +48,6 @@ class FClashVPNService : VpnService() {
         intent?.let {
             when (it.action) {
                 "start" -> {
-//                    stopVpnService()
                     startVpnService()
                     return START_STICKY
                 }
@@ -110,7 +94,6 @@ class FClashVPNService : VpnService() {
 
     private fun stopVpnService() {
         try {
-            // Close the VPN interface
             mFd?.close()
             Log.d(TAG, "fclash service stopped")
         } catch (e: Exception) {
